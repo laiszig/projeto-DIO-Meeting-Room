100a56ea18660c14436de365e4068729ad5fb73a
Lais Ziegler <laisziegler@gmail.com>
2021-09-05 16:41:33 -0300

    Initial commit

---
+++ src/main/java/dio/crud/saladereuniao/exception/ErrorDetails.java
@@ -0,0 +1,21 @@
+package dio.crud.saladereuniao.exception;
+
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class ErrorDetails {
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    private Date timestamp;
+    private String message;
+    private String details;
+}
